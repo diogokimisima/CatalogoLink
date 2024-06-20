588cc1673b976cfbc2cdf68bad6162468388f6c9
diogokimisima <dkimisima@gmail.com>
2024-06-20 13:43:04 -0300

    arrumando bugs

--- src/components/header/HeaderCategorias.vue
+++ src/components/header/HeaderCategorias.vue
@@ -1,6 +1,6 @@
 //HeaderCategorias
 <template>
-    <div class="bg-blue-950 text-gray-100 overflow-y-auto flex items-center h-16">
+    <div class="bg-blue-950 text-gray-100 overflow-y-auto flex items-center h-20">
         <ul class="flex flex-row">
             <li class="px-6 py-2 hover:bg-gray-900 rounded">
                 <a href="#" class="block">Todos</a>
