e88dfc4e906efeb70e1e96750ac5748e8fad1ecd
diogokimisima <dkimisima@gmail.com>
2024-06-20 13:42:23 -0300

    arrumando a altura do headercategoria

--- src/components/header/HeaderCategorias.vue
+++ src/components/header/HeaderCategorias.vue
@@ -1,6 +1,6 @@
 //HeaderCategorias
 <template>
-    <div class="bg-blue-950 text-gray-100 overflow-y-auto flex items-center h-24">
+    <div class="bg-blue-950 text-gray-100 overflow-y-auto flex items-center h-16">
         <ul class="flex flex-row">
             <li class="px-6 py-2 hover:bg-gray-900 rounded">
                 <a href="#" class="block">Todos</a>
