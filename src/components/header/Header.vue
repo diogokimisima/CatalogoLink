018a3685a24c4247f4d8038f40a18d6100d64014
diogokimisima <dkimisima@gmail.com>
2024-06-25 16:13:12 -0300

    arrumando bug

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -84,7 +84,7 @@
                     <ul v-if="displayStates.size" class="flex flex-row flex-wrap gap-1 my-3">
                         <li v-for="tamanho in uniqueSizes" :key="tamanho">
                             <button
-                                :class="['mb-2 border bg-white border-neutral-300 w-14 h-8 rounded-md', filterSelected === tamanho ? 'border-black' : '']"
+                                :class="['mb-2 border bg-white border-black border-opacity-20 w-14 h-8 rounded-md', filterSelected === tamanho ? 'border-opacity-100' : '']"
                                 @click="filterSize(tamanho)">
                                 <div class="flex items-center justify-center">
                                     {{ tamanho }}
